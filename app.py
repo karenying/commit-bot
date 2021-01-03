236e547757cf3f858bcf5f5cf2138e2a17178506
Karen Ying <karenying7@gmail.com>
2021-01-03 13:01:14 -0500

    Rename files

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from random import randint
+
+MAX_COMMITS = 5
+THRESHOLD = 3
+
+def commit():
+  os.system("echo $(date) >> output.txt")
+  os.system("git add output.txt")
+  os.system("git commit -m Update output.txt")
+  os.system("git push")
+
+""" if (randint(0, 10) > THRESHOLD):
+  for i in range(randint(0, MAX_COMMITS)):
+    commit() """
+
+commit()
