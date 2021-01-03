3d2a364f3ad6c025c00dee4b7d93d0cb5478e184
Karen Ying <karenying7@gmail.com>
2021-01-03 13:27:41 -0500

    Clear up output.txt

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import os
+from os import system
 from random import randint
 
 MAX_COMMITS = 3
 THRESHOLD = 3
 
 def commit():
-  os.system("echo $(date) >> output.txt")
-  os.system("git add output.txt")
-  os.system("git commit -m Update output.txt")
-  os.system("git push")
+  system("echo $(date) >> output.txt")
+  system("git add output.txt")
+  system("git commit -m Update output.txt")
+  system("git push")
 
 if (randint(0, 10) > THRESHOLD):
   for i in range(randint(0, MAX_COMMITS)):
